63da808017878667671b97a8aef75e038f391ee4
inclinedadarsh <dubeyadarshmain@gmail.com>
2023-08-17 12:49:47 +0530

    feat(frontend): :sparkles: add nav bar and style for it

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,63 @@
+'use client';
+
+import Link from 'next/link';
+
+const links = [
+	{
+		id: 1,
+		title: 'Home',
+		url: '/',
+	},
+	{
+		id: 2,
+		title: 'Portfolio',
+		url: '/portfolio',
+	},
+	{
+		id: 3,
+		title: 'Blog',
+		url: '/blog',
+	},
+	{
+		id: 4,
+		title: 'About',
+		url: '/about',
+	},
+	{
+		id: 5,
+		title: 'Contact',
+		url: '/contact',
+	},
+	{
+		id: 6,
+		title: 'Dashboard',
+		url: '/dashboard',
+	},
+];
+
 const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className="flex justify-between items-center">
+			<Link href="/" className="font-bold text-lg">
+				Solstice
+			</Link>
+			<ul className="flex gap-5 items-center">
+				{links.map((item) => (
+					<li>
+						<Link href={item.url} key={item.id}>
+							{item.title}
+						</Link>
+					</li>
+				))}
+				<button
+					type="button"
+					className="px-5 py-3 font-semibold border-2 rounded-lg text-sm border-secondary-bg hover:bg-secondary-bg dark:border-secondary-bg--dark dark:hover:bg-secondary-bg--dark"
+				>
+					Logout
+				</button>
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,14 @@ module.exports = {
 		extend: {},
 		colors: {
 			'primary-bg': '#fff',
-			'primary-text': '#111',
-			'primary-bg--dark': '#111',
-			'primary-text--dark': '#ddd',
+			'secondary-bg': '#f0f0f0',
+			'primary-text': '#0f172a',
+			'secondary-text': '#516585',
+
+			'primary-bg--dark': '#0f172a',
+			'secondary-bg--dark': '#1e293b',
+			'primary-text--dark': '#fff',
+			'secondary-text--dark': '#94a3b8',
 		},
 	},
 	plugins: [],
